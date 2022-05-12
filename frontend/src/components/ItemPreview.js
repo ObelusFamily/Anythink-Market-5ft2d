57ae5b916e82d7cfd209195c1991127daab8681a
galnir <nirnir122@gmail.com>
2022-05-12 22:04:26 +0300

    placeholder when theres no image

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -39,6 +39,7 @@ const ItemPreview = (props) => {
         src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
+        placeholder={item.image ?? '/placeholder.png'}
       />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
