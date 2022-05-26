8d798d19284fb8dfe786a0e9393a375be18a857b
galnir <nirnir122@gmail.com>
2022-05-26 18:37:53 +0300

    empty

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { store, history } from "./store";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
-
+// n
 import App from "./components/App";
 
 ReactDOM.render(
