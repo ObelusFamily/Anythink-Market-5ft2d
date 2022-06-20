0636cfe7500a61b83b67b3a65ec3e3a690758c8b
galnir <nirnir122@gmail.com>
2022-06-20 21:51:14 +0300

    searchbox appear on clicking get

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -2,22 +2,26 @@ import React from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = ({ title, setTitle, itemNotFound }) => {
-  console.log("itemnotfound", itemNotFound);
+  const [showBanner, setShowBanner] = React.useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            value={itemNotFound === true ? "" : title}
-            type="text"
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            onChange={(e) => {
-              return setTitle(e.target.value);
-            }}
-          />
+          <span id="get-part">
+            A place to <span onClick={() => setShowBanner(true)}>get</span>
+          </span>
+          {showBanner ? (
+            <input
+              value={itemNotFound === true ? "" : title}
+              type="text"
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              onChange={(e) => {
+                return setTitle(e.target.value);
+              }}
+            />
+          ) : null}
           <span> the cool stuff.</span>
         </div>
       </div>
